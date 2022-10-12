22e1f487d063f140deca476720d136815985cf0d
ValeriyN <negriyko@gmail.com>
2022-10-12 09:14:41 -0400

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,43 @@
+"""Translation module."""
+import os
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+
+def englishToFrench(english_text = ''):
+    '''English to Franch translation'''
+    if english_text == '':
+        return ''
+
+    french_text = language_translator.translate(
+        text=english_text,
+        model_id='en-fr').get_result()['translations'][0]['translation']
+    return french_text
+
+
+def frenchToEnglish(french_text = ''):
+    '''Franch to English translation'''
+    if french_text == '':
+        return french_text
+
+    english_text = language_translator.translate(
+        text=french_text,
+        model_id='fr-en').get_result()['translations'][0]['translation']
+    return english_text
+
+print(englishToFrench("Hello"))
